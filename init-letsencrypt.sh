2f43b99936d0a56c07433f123d4197e0bfdcc035
Calvin <calvinho125@gmail.com>
2021-06-18 03:22:15 -0400

    deployment

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,9 +7,9 @@ fi
 
 domains=(hk-gardento.com) # www.example.org)
 rsa_key_size=4096
-data_path="./deploy/certbot"
+data_path="./data/certbot"
 email="calvinho125@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -39,7 +39,7 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+docker-compose -f docker-compose.yml up --force-recreate -d service_name
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
@@ -77,4 +77,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+docker-compose exec service_name nginx -s reload
